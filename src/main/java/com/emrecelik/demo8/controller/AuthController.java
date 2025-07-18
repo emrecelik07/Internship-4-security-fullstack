197caaa7e9cecb4d802b6eaeab7fc4339f99e1cb
emrecelik07 <emrcel07@gmail.com>
2025-07-18 23:28:42 +0300

    logout added

--- src/main/java/com/emrecelik/demo8/controller/AuthController.java
+++ src/main/java/com/emrecelik/demo8/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.emrecelik.demo8.io.ResetPasswordRequest;
 import com.emrecelik.demo8.service.ProfileService;
 import com.emrecelik.demo8.service.impl.AppUserDetailsService;
 import com.emrecelik.demo8.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -137,6 +138,22 @@ public class AuthController {
         }catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
+
+    }
+
+    @PostMapping(path = "/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+         ResponseCookie cookie = ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body("Logged out successfully!");
 
     }
 }
